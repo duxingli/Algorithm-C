bd1e0a2ff8c0664e10748ad5bd2eab2222f66f85
duxingli <1664886277@qq.com>
2020-09-08 11:13:51 +0800

    求2个N阶方阵的乘积

---
+++ 求2个N阶方阵的乘积.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define n 3
+void MatrixMultiply(int A[n][n], int B[n][n], int C[n][n])
+{
+	int i, j, k;
+	for(i=0; i<n; i++){
+		for(j=0; j<n; j++){
+			C[i][j] = 0;
+			for(k=0; k<n; k++){
+				C[i][j] = C[i][j] + A[i][k] * B[k][j]; 
+			}
+		}
+	}
+}
+void Print(int X[n][n])
+{
+	printf("C = A x B的值为：\n");
+	int i,j;
+	for(i=0; i<n; i++){
+		for(j=0; j<n; j++){
+			printf("%5d",X[i][j]);
+		}
+		printf("\n");
+	}
+}
+int main(void)
+{
+	int A[n][n],B[n][n],C[n][n];
+	printf("请输入A方阵：\n");
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			scanf("%d",&A[i][j]);
+		}
+	}
+	printf("请输入B方阵：\n");
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			scanf("%d",&B[i][j]);
+		}
+	}
+	MatrixMultiply(A, B, C);
+	Print(C);
+	return 0;
+}
